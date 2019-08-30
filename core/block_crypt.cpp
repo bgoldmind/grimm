cd864e509947bd8e7c174b69317b3a1f34b3db4e
Freenetcoder <zandrewboyle@gmail.com>
2019-08-30 11:16:07 +0100

    safe signature

--- core/block_crypt.cpp
+++ core/block_crypt.cpp
@@ -1021,7 +1021,9 @@ namespace grimm
 			<< (uint32_t) Block::PoW::N
 			<< (uint32_t) Block::PoW::NonceType::nBits
 			<< uint32_t(16) // increment this whenever we change something in the protocol
-
+#ifndef GRIMM_TESTNET1
+            << "masternet"
+#endif
 			// out
 			>> pForks[0].m_Hash;
 
