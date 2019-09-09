891509a9e48ba26f5af471e01669cc54434bfdd5
Freenetcoder <zandrewboyle@gmail.com>
2019-09-09 18:50:56 +0100

    ver. update + improvements

--- wallet/wallet.cpp
+++ wallet/wallet.cpp
@@ -896,7 +896,7 @@ namespace grimm::wallet
 
         m_WalletDB->rollbackConfirmedUtxo(sTip.m_Height);
 
-        ResumeAllTransactions();
+
 
         for (auto it = m_ActiveTransactions.begin(); m_ActiveTransactions.end() != it; it++)
         {
@@ -905,6 +905,23 @@ namespace grimm::wallet
             if (pTx->Rollback(sTip.m_Height))
             {
                 UpdateOnSynced(pTx);
+            }
+        }
+
+        // Rollback inactive (completed or active) transactions if applicable
+        auto txs = m_WalletDB->getTxHistory(TxType::ALL); // get list of ALL transactions
+        for (auto& tx : txs)
+        {
+            // For all transactions that are not currently in the 'active' tx list
+            if (m_ActiveTransactions.find(tx.m_txId) == m_ActiveTransactions.end())
+            {
+                // Reconstruct tx with reset parameters and add it to the active list
+                auto pTx = constructTransaction(tx.m_txId, tx.m_txType);
+                if (pTx->Rollback(sTip.m_Height))
+                {
+                    m_ActiveTransactions.emplace(tx.m_txId, pTx);
+                    UpdateOnSynced(pTx);
+                }
             }
         }
 
